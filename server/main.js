e771f8a498a56b52d136f5941d5c6bf2306f6d38
kentonambara <kentonambara@gmail.com>
2020-10-22 09:05:52 -0400

    Heroku

--- server/main.js
+++ server/main.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  const testing = [{ id: 1, name: "Test" }];
+// app.get("/", (req, res) => {
+//   const testing = [{ id: 1, name: "Test" }];
 
-  res.json(testing);
+//   res.json(testing);
+// });
+
+// const port = 5000;
+
+// app.listen(port, () => console.log(`Server started on port ${port}`));
+
+const path = require("path");
+const publicPath = path.join(__dirname, "..", "client/build");
+const port = process.env.PORT || 5000;
+
+app.use(express.static(publicPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
-const port = 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
