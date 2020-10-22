bb20262ae52a7f073c5498a83f6bd6180327d534
kentonambara <kentonambara@gmail.com>
2020-10-22 09:21:43 -0400

    Build

---
+++ client/build/precache-manifest.154f94e00a5e0f4df3f7f89f69be784e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdda7375d5d45c0c719163f43254bdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf8a67f650b82f9f5f48",
+    "url": "/static/js/2.96548c66.chunk.js"
+  },
+  {
+    "revision": "569f33c8d1d96bc5b64a6ce10183922b",
+    "url": "/static/js/2.96548c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a040ec16eb3c47d9a111",
+    "url": "/static/js/main.79cd0a90.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
